68872c9d1b970bdf786261f95646eb6cc54fbf2e
Alex Mihov <alexmihov.as@gmail.com>
2025-01-14 18:51:57 +0100

    feat selezione elementi

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 // selezione degli elementi
+const countdown = document.getElementById("countdown");
+const numbersList = document.getElementById("numbers-list");
+const form = document.getElementById("answers-form");
+const inputElements = document.querySelectorAll("form-control");
+const message = document.getElementById("message");
 
+console.log(countdown, numbersList, form, inputElements, message);
 
 // gestione dei numeri casuali
-
+numeri
 
 // gestione del countdown
 
